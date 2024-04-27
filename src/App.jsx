82dfd4141a2ff58cc5579a936b3037e1003acd33
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-04-27 10:18:40 +0200

    add rows and resp

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MyNavbar from "./components/MyNavbar";
 import MyFooter from "./components/MyFooter";
 import HeadingMain from "./components/HeadingMain";
 import FilmRows from "./components/FilmRows";
+import { Row } from "react-bootstrap";
 
 function App() {
   return (
@@ -12,7 +13,11 @@ function App() {
       <header className="App-header">
 <MyNavbar/>
 <HeadingMain/>
+<Row className="row-cols-2 row-cols-md-4 row-cols-lg-6"> 
 <FilmRows nameOfFilm = "Harry Potter"/>
+<FilmRows nameOfFilm= "Star Wars"/>
+<FilmRows nameOfFilm= "Batman"/>
+</Row>
 <MyFooter/>
       </header>
     </div>
